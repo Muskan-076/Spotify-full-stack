1058674652ccb51da3062a12fa374beccca8f0a4
Muskan076 <muskangupta252006@gmail.com>
2026-07-04 22:13:34 +0530

    added some API

---
+++ spotify_backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(express.json());
+app.use(cors());   // using this to connect our backend with frontend if these two are running on different port numbers then cors connects them.
+
+//initializing routes
+app.use("/api/song", songRouter)
+
+app.get('/',(req,res)=> res.send("API Working"))
+
+app.listen(port,()=>console.log(`Server started on ${port}`));
+
+// we have created basic API till now
