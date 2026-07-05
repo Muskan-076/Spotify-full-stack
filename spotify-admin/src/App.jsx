05f35535f98d41a2b30705e7cb3d051a641ef35c
Muskan076 <muskangupta252006@gmail.com>
2026-07-06 01:30:22 +0530

    added few functionality in Add Song section of admin panel.

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -6,6 +6,7 @@ import AddAlbum from './pages/AddAlbum';
 import ListSong from './pages/ListSong';
 import ListAblum from './pages/ListAblum';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
       <ToastContainer />
       <Sidebar />
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
-
+        <Navbar />
         <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
           <Routes>
             <Route path='/add-song' element={<AddSong />} />
